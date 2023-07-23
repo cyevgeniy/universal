1dff40b6e73457cbc7957954e97a1fdabb426e57
Evgeny Chaban <ychbn@ychbn.com>
2023-07-23 17:08:23 +0300

    feat(11ty): add quote shortcode

--- ssg/11ty/eleventy.config.js
+++ ssg/11ty/eleventy.config.js
@@ -6,61 +6,62 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 // Config based on https://github.com/11ty/eleventy-base-blog/blob/main/eleventy.config.js
 module.exports = function (eleventyConfig) {
 
-	eleventyConfig.setLibrary('md', markdownIt);
+    eleventyConfig.setLibrary('md', markdownIt);
 
-	eleventyConfig.addPlugin(bundlerPlugin);
+    eleventyConfig.addPlugin(bundlerPlugin);
 
-	// Get the first `n` elements of a collection.
-	eleventyConfig.addFilter("head", (array, n) => {
-		if(!Array.isArray(array) || array.length === 0) {
-			return [];
-		}
+    // Get the first `n` elements of a collection.
+    eleventyConfig.addFilter("head", (array, n) => {
+        if(!Array.isArray(array) || array.length === 0) {
+            return [];
+        }
 
-		return n < 0 ? array.slice(n) : array.slice(0, n);
-	});
+        return n < 0 ? array.slice(n) : array.slice(0, n);
+    });
 
-	eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-	// Universal Shortcodes (Adds to Liquid, Nunjucks, JavaScript, Handlebars)
-	eleventyConfig.addPairedShortcode(
-		"fullbleed",
-		shortcodes.bleed
-	);
+    // Universal Shortcodes (Adds to Liquid, Nunjucks, JavaScript, Handlebars)
+    eleventyConfig.addPairedShortcode(
+        "fullbleed",
+        shortcodes.bleed
+    );
 
-	eleventyConfig.addPairedShortcode("container", shortcodes.container);
+    eleventyConfig.addPairedShortcode("container", shortcodes.container);
 
-	/**
-	 * card-grid shortcode is a container for cards.
-	 * It automatically places cards and adds spacing between them.
-	 */
-	eleventyConfig.addPairedShortcode("card-grid", shortcodes.cardGrid);
+    /**
+     * card-grid shortcode is a container for cards.
+     * It automatically places cards and adds spacing between them.
+     */
+    eleventyConfig.addPairedShortcode("card-grid", shortcodes.cardGrid);
 
 
-	/**
-	 * card shortcode is a card container.
-	 */
-	eleventyConfig.addPairedShortcode(
-		"card",
-		shortcodes.card
-	);
+    /**
+     * card shortcode is a card container.
+     */
+    eleventyConfig.addPairedShortcode(
+        "card",
+        shortcodes.card
+    );
 
-	eleventyConfig.addShortcode("figure", shortcodes.figure);
-	eleventyConfig.addShortcode("button", shortcodes.button);
+    eleventyConfig.addShortcode("figure", shortcodes.figure);
+    eleventyConfig.addShortcode("button", shortcodes.button);
+    eleventyConfig.addShortcode("quote", shortcodes.quote);
 
-	eleventyConfig.addPairedShortcode(
-		"aside",
-		shortcodes.aside
-	);
+    eleventyConfig.addPairedShortcode(
+        "aside",
+        shortcodes.aside
+    );
 
-	eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("img");
 
-	return {
-		// These are all optional:
-		dir: {
-			input: "content", // default: "."
-			includes: "../_includes", // default: "_includes"
-			data: "../_data", // default: "_data"
-			output: "_site",
-		},
-	};
+    return {
+        // These are all optional:
+        dir: {
+            input: "content", // default: "."
+            includes: "../_includes", // default: "_includes"
+            data: "../_data", // default: "_data"
+            output: "_site",
+        },
+    };
 };
